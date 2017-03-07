c6d3fcb37609dc98fdd52b6a5f12409089c7bc8b
nvietri <nickvietri@gmail.com>
2017-03-07 09:39:56 -0600

    asdf

---
+++ Print.java
@@ -0,0 +1,19 @@
+public String toString()
+{
+	for(int i = 0; i < magSq.length; i++)
+	{
+		for(int k = 0; k < magSq.length;k++)
+		{
+		System.out.print(magsq[i][k] + " ");
+		}
+		System.out.println();
+		}
+		return "";
+	
+
+
+
+
+
+
+}
